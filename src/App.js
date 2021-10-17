47a5ea2d4f844435d19be265a5d5f20a7d96b5a0
saifulislammunna <saifulmunna28@gmail.com>
2021-10-18 00:25:09 +0600

    start

---
+++ src/App.js
@@ -0,0 +1,16 @@
+ 
+import { BrowserRouter } from 'react-router-dom';
+import './App.css';
+import Header from './components/Header/Header';
+
+function App() {
+  return (
+    <div className="App">
+        <BrowserRouter>
+          <Header></Header>
+        </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC5pvTf_bo9vkYgMH6M9WOd7rjGxyB382A",
+    authDomain: "bangladesh-general-hospital.firebaseapp.com",
+    projectId: "bangladesh-general-hospital",
+    storageBucket: "bangladesh-general-hospital.appspot.com",
+    messagingSenderId: "806251550408",
+    appId: "1:806251550408:web:5a8b590c36d4510d3af7fd"
+  };
+
+  export default firebaseConfig;
