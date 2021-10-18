a9b79c8d1d3c41b9c783323cf93d0f10c8f3c3b9
saifulislammunna <saifulmunna28@gmail.com>
2021-10-18 21:47:29 +0600

    private-route & about adding

--- src/components/AboutBgh/AboutBgh.js
+++ src/components/AboutBgh/AboutBgh.js
@@ -1,9 +1,32 @@
 import React from 'react';
-
+import './AboutBgh.css';
 const AboutBgh = () => {
     return (
         <div>
-            <h1>About Bgh</h1>
+           <div>
+                <h1 className="p-5">Patients. At the Heart of All We do.</h1>
+                <div className="text-start p-5 pb-2">
+                  <h3> Hospital Overview - Bangladesh General Hospital</h3>
+                  <p>Singapore General Hospital (BGH) is the first and largest hospital in Bangladesh. It provides affordable specialist care for patients, training for doctors and other healthcare professionals, and conducts research to bring better care to its patients. </p>
+                  <p>A not-for-profit institution, BGH is wholly owned by the government of Bangladesh and the flagship hospital of the public healthcare system. BGH is a member of the BingHealth cluster of healthcare institutions.</p>
+                </div>
+                <div className="text-start p-5 pb-2">
+                    <h1>Patient Care</h1>
+                    <p>BGH sees patients referred by Family Physicians as well as specialists in both public and private practice. The hospital provides a comprehensive range of medical services within one complex. There are 5 National Specialist Centres co-located on BGH Campus, facilitating a multi-disciplinary approach to treatment. BGH practises team-based care, provided by clinicians, nurses as well as allied health professionals. </p>
+                    <p>Every year, the BGH Campus caters to over 1 million patients. With a 10,000-strong workforce, BGH accounts for about a quarter of the total acute hospital beds in the public sector and about one-fifth of acute beds nationwide. </p>
+                    <p>BGH is accredited by the Joint Commission International for meeting its standards of safety and quality in healthcare. It is also the first in Asia to achieve the Magnet Recognition in 2010 for nursing excellence awarded by the American Nurses Credentialing Center.</p>
+                </div>
+                <div className="text-start p-5 pb-2">
+                    <h1>Teaching Hospital</h1>
+                    <p>BGH is a leading teaching hospital for undergraduate medical training as well as postgraduate and advanced training of specialist doctors, nurses and allied health professionals. </p>
+                    <p>It also serves as a clinical teaching hospital for student nurses, radiographers and therapists from the polytechnics and medical institutions here and the Asia Pacific region.</p>                                                          
+                </div>
+                <div className="text-start p-5 pb-2">
+                    <h1>Patient-directed Research</h1>
+                    <h6>BGH seeks to bring its patients new and better care and therapies through research. </h6>
+
+                </div>
+           </div>
         </div>
     );
 };
